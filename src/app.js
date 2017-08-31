da1abaee556d9834cbf36ced5b18190356d3d865
iozeen <iozintv@gmail.com>
2017-08-31 23:23:20 +0300

    initial setup, added loader

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const css = require('./styles/app.scss');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello</div>;
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "dist"),
+    stats: 'errors-only',
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin("app.css"),
+  ]
+}
